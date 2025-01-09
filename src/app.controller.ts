feb6326df4f7f3a4b5ea1bc34ae9d73af17903cd
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2025-01-09 13:19:28 -0300

    base routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,34 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hola Mundo';
+  }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'yo soy nuevo';
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products limit=>${limit} offset=>${offset} brand=> ${brand}`;
+  }
+
+  @Get('products/filter')
+  getProductFilter() {
+    return `producto con un bello filtro de datos`;
+  }
+  @Get('products/:producId')
+  getProduct(@Param('producId') producId: number) {
+    return `product ${producId}`;
+  }
+
+  @Get('categories/:id/products/:producId')
+  getCategory(@Param('producId') producId: number, @Param('id') id: number) {
+    return `product ${producId} and id ${id}`;
   }
 }
---
+++ src/recap.ts
@@ -0,0 +1,10 @@
+const myName = 'Guillermo';
+const myAge = 34;
+console.log('My name is', myName);
+console.log('My age is', myAge);
+
+const suma = (a: number, b: number) => {
+  return a + b;
+};
+
+suma(12, 12);
